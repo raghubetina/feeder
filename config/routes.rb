a0e1184b338d7a6c79ed1a961790b4842f509051
Raghuveera Betina <rbetina@gmail.com>
2011-11-16 15:11:06 -0600

    Created users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Feeder::Application.routes.draw do
+  get "users/new"
+
+  get "users/show"
+
+  get "users/edit"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
