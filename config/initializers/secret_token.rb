5866ab35f4e6ef0d8c013ba6881bf68bbe6fc803
Raghuveera Betina <rbetina@gmail.com>
2011-11-16 15:03:15 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feeder::Application.config.secret_token = '7cc2ccafb6a02ad7141edadbb01d7a6055348a6018b86ae463b17f29df3f286e052db5771cc16ede32ef6b1d39a381cdf34d15e55f4d46f55fa9daf803b4c812'
